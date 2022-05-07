2b94234a2c408225f882aa2b6f52edaa235d7f25
Hasan <hasanuzzaman201601@gmail.com>
2022-05-08 00:10:08 +0600

    clock updated

--- src/Components/Timer/Timer.js
+++ src/Components/Timer/Timer.js
@@ -1,53 +1,84 @@
-import React, { useEffect, useState } from 'react'
-import "./Timer.css"
-
-const calculateTimeLeft = () => {
-  let year = new Date().getFullYear();
-  const difference = +new Date(`10/01/${year}`) - +new Date();
-  let timeLeft = {};
-
-  if (difference > 0) {
-    timeLeft = {
-      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-      minutes: Math.floor((difference / 1000 / 60) % 60),
-      seconds: Math.floor((difference / 1000) % 60)
-    };
-  }
-  return timeLeft;
-};
+import React, { useEffect, useState } from "react";
+import "./Timer.css";
 
 const Timer = () => {
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-  console.log(timeLeft);
-
+  const [countdownDate, setCountdownDate] = useState(
+    new Date("05/18/2022").getTime()
+  );
+  const [state, setState] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
-    return () => clearTimeout(timer);
-  });
-  const timerComponents = [];
+    setInterval(() => setNewTime(), 1000);
+  }, []);
 
-  Object.keys(timeLeft).forEach((interval) => {
-    
-    if (!timeLeft[interval]) {
-      return;
+  const setNewTime = () => {
+    if (countdownDate) {
+      const currentTime = new Date().getTime();
+
+      const distanceToDate = countdownDate - currentTime;
+
+      let days = Math.floor(distanceToDate / (1000 * 60 * 60 * 24));
+      let hours = Math.floor(
+        (distanceToDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      let minutes = Math.floor(
+        (distanceToDate % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      let seconds = Math.floor((distanceToDate % (1000 * 60)) / 1000);
+
+      const numbersToAddZeroTo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+      days = `${days}`;
+      if (numbersToAddZeroTo.includes(hours)) {
+        hours = `0${hours}`;
+      } else if (numbersToAddZeroTo.includes(minutes)) {
+        minutes = `0${minutes}`;
+      } else if (numbersToAddZeroTo.includes(seconds)) {
+        seconds = `0${seconds}`;
+      }
+
+      setState({ days: days, hours: hours, minutes, seconds });
     }
-
-    timerComponents.push(
-      <span className='border-solid border-gray-200 drop-shadow-2xl border-2 m-2 p-2 rounded bg-gradient-to-r from-sky-500 to-indigo-500'>
-        <span className='d-flex '>{timeLeft[interval]}<span> {interval}{" "}</span></span>
-      </span>
-    );
-  });
+  };
 
   return (
-    <div className='my-5'>
-    <h1 className='text-white font-bold text-3xl mb-3 my-5'>{timerComponents.length ? timerComponents : <span>Time's up!</span>}</h1>
-  </div>
-  )
-}
+    <div className="container mx-auto rounded-lg mt-2 timer">
+      <h2 className="subtitle">Days Until Launchpad</h2>
+      <div className="countdown-wrapper">
+        <div className="time-section">
+          <div className="time">
+            {state.days || "0"} <small className="time-text">Days</small>
+          </div>
+        </div>
+        <div className="time-section">
+          {/* <div className="time">:</div> */}
+        </div>
+        <div className="time-section">
+          <div className="time">{state.hours || "00"}</div>
+          {/* <small className="time-text">Hours</small> */}
+        </div>
+        <div className="time-section">
+          <div className="time">:</div>
+        </div>
+        <div className="time-section">
+          <div className="time">{state.minutes || "00"}</div>
+          {/* <small className="time-text">Minutes</small> */}
+        </div>
+        <div className="time-section">
+          <div className="time">:</div>
+        </div>
+        <div className="time-section">
+          <div className="time">{state.seconds || "00"}</div>
+          {/* <small className="time-text">Seconds</small> */}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Timer
+export default Timer;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import According from "../../Components/According/According";
 import Featured from "../../Components/Featured/Featured";
 import Innovate from "../../Components/Innovate/Innovate";
 import Mint from "../../Components/Mint/Mint";
+import Redeem from "../../Components/Redeem/Redeem";
 import Footer from "../../Components/Shared/Footer";
 import Nav from "../../Components/Shared/Nav";
 import Tokenomics from "../../Components/Tokenomics/Tokenomics";
@@ -10,23 +11,23 @@ import Hero from "./Hero/Hero";
 import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className="main_home" id="home">
-            <div className="hero_bg">
-                <Nav />
-                <div className="hero_text">
-                    <Hero />
-                    <Featured />
-                    <Innovate />
-                    <Mint />
-                    <Tokenomics />
-                    <According />
+  return (
+    <div className="main_home" id="home">
+      <div className="hero_bg">
+        <Nav />
+        <div className="hero_text">
+          <Hero />
+          <Featured />
+          <Innovate />
+          {/* <Redeem /> */}
+          <Tokenomics />
+          <According />
 
-                    <Footer />
-                </div>
-            </div>
+          <Footer />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
